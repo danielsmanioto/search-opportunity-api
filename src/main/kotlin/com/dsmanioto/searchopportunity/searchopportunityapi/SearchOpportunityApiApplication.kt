f3e5ddac7c5325609d8e9f3900d94d61fb9489f2
Daniel Smanioto <daniel.smanioto@gmail.com>
2019-09-14 21:47:20 -0300

    initial commit

---
+++ src/main/kotlin/com/dsmanioto/searchopportunity/searchopportunityapi/SearchOpportunityApiApplication.kt
@@ -0,0 +1,11 @@
+package com.dsmanioto.searchopportunity.searchopportunityapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SearchOpportunityApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SearchOpportunityApiApplication>(*args)
+}
